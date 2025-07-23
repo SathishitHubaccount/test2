6dc9d5e7ceac78d235f48b19125c592fa9c9b8b0
SathishitHubaccount <sathish2352004@gmail.com>
2025-07-23 11:36:05 +0530

    v3

---
+++ prime.py
@@ -0,0 +1,15 @@
+def prime(n):
+    """Return a list of prime numbers up to n."""
+    primes = []
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+
+n = int(input("Enter a number: "))
+print("Prime numbers from 0 to", n, "are:", prime(n))
