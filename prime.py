616eec3cf79a6e72bd2408d5debff3efb336e705
SathishitHubaccount <sathish2352004@gmail.com>
2025-07-23 12:03:40 +0530

    check

--- prime.py
+++ prime.py
@@ -1,6 +1,6 @@
 def prime(n):
     """Return a list of prime numbers up to n."""
-    primes = []
+    primes = set()
     for num in range(2, n + 1):
         is_prime = True
         for i in range(2, int(num**0.5) + 1):
